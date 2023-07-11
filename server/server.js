029bc81a46375bee30f48bd0a0a1206f0a6af873
isi <isabelldorendorf99@web.de>
2023-07-11 17:45:04 +0200

    restructuring project

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const postRoutes = require('./routes/post.routes');
+const cors = require('cors')
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+// enable cors for all requests
+app.use(cors());
+app.use('/posts', postRoutes);
+
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`server running on http://localhost:${PORT}`);
+    }
+});
