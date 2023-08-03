159dc1a061b1d42b500a3cf77617e72ccfc0506c
isi <isabelldorendorf99@web.de>
2023-08-03 09:48:31 +0200

    created pop-up-windows. adding post and viewing post

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,3 @@
-// JavaScript-Code für den Slider
-const slider = document.querySelector('.slider');
-let isDragging = false;
-let startPosition = 0;
-let currentTranslate = 0;
-let prevTranslate = 0;
-
-slider.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startPosition = e.clientX;
-});
-
-slider.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-    const currentPosition = e.clientX;
-    const slideWidth = slider.querySelector('.slide').clientWidth;
-    currentTranslate = prevTranslate + currentPosition - startPosition;
-    slider.style.transform = `translateX(${currentTranslate}px)`;
-});
-
-slider.addEventListener('mouseup', () => {
-    isDragging = false;
-    const slideWidth = slider.querySelector('.slide').clientWidth;
-
-    // Snapping to the nearest slide
-    const snapToSlide = Math.round(currentTranslate / slideWidth) * slideWidth;
-    prevTranslate = snapToSlide;
-    slider.style.transform = `translateX(${snapToSlide}px)`;
-});
-
-slider.addEventListener('mouseleave', () => {
-    isDragging = false;
-    const slideWidth = slider.querySelector('.slide').clientWidth;
-    prevTranslate = currentTranslate;
-    slider.style.transform = `translateX(${prevTranslate}px)`;
-});
-
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
@@ -46,9 +9,17 @@ if ('serviceWorker' in navigator) {
         );
 }
 function toggleMenu() {
-    const menu = document.getElementsByClassName("menu-Button");
-    const button = document.getElementsByClassName("menu");
-    menu.onclick = function() {
-        button.classList.toggle("show");
-    }
+    var navigation = document.getElementById("hidden-menu");
+    navigation.classList.toggle("show");
+}
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+function showPost() {
+    var onePost = document.getElementById("onePost");
+    console.log("test");
+    onePost.classList.toggle("showOnePost");
 }
